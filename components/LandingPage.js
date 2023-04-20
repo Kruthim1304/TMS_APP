091dbaa7c976cb73616c9e011f8e1f3278525617
Atharva Patil <atharvapatil0052@gmail.com>
2023-04-20 20:28:18 +0530

    API integrated

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { View, Text, StyleSheet, TextInput, FlatList, TouchableOpacity } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import axios from 'axios';
+import { ScrollView } from 'react-native-web';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 
-const trucksData = [
-    { truckNumber: 'Truck 1', truckOrder: 'Order 123', currentLocation: 'Location 1', currentStatus: 'Status 1' },
-    { truckNumber: 'Truck 2', truckOrder: 'Order 456', currentLocation: 'Location 2', currentStatus: 'Status 2' },
-    { truckNumber: 'Truck 3', truckOrder: 'Order 789', currentLocation: 'Location 3', currentStatus: 'Status 3' },
-  ];
+
+// const trucksData = [
+//     { truckNumber: 'Truck 1', truckOrder: 'Order 123', currentLocation: 'Location 1', currentStatus: 'Status 1' },
+//     { truckNumber: 'Truck 2', truckOrder: 'Order 456', currentLocation: 'Location 2', currentStatus: 'Status 2' },
+//     { truckNumber: 'Truck 3', truckOrder: 'Order 789', currentLocation: 'Location 3', currentStatus: 'Status 3' },
+//   ];
 
 const LandingPage = () => {
     const [searchValue, setSearchValue] = React.useState('');
     const [filteredTrucks, setFilteredTrucks] = React.useState(trucksData);
     const navigation = useNavigation();
+    const [trucksData, setTrucksData] = useState([]);
+
+    // useEffect(() => {
+    //   axios
+    //     .get('http://zingtrack.com/GPSRestWebService/rest/GetOnlineData/json?API_KEY=CST22ZTRNIPLMRAPI&VEHICLE_ID=ALL')
+    //     .then((response) => {
+    //       // Set fetched trucks data to state
+    //       setTrucksData(response.data);
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error fetching trucks data:', error);
+    //     });
+    // }, []);
+
+
+
+    useEffect(() => {
+      const fetchTrucksData = async () => {
+        try {
+          const response = await fetch('http://zingtrack.com/GPSRestWebService/rest/GetOnlineData/json?API_KEY=CST22ZTRNIPLMRAPI&VEHICLE_ID=ALL'); 
+          
+          const data = await response.json();
+          setTrucksData(data);
+        } catch (error) {
+          console.error('Failed to fetch truck data', error);
+        }
+      };
+
+      fetchTrucksData();
+    }, []);
   
     const handleSearch = (text) => {
       setSearchValue(text);
@@ -34,15 +68,29 @@ const LandingPage = () => {
     //     </View>
     //   );
     // }
+
+
+    // const renderTruckRow = ({ item }) => (
+    //     <TouchableOpacity style={styles.truckRow} onPress={() => handleTruckPress(item.id)}>
+    //       <Text style={styles.truckNumber}>{item.truckNumber}</Text>
+    //       <Text style={styles.truckOrder}>{item.truckOrder}</Text>
+    //       <Text style={styles.currentLocation}>{item.currentLocation}</Text>
+    //       <Text style={styles.currentStatus}>{item.currentStatus}</Text>
+    //     </TouchableOpacity>
+    //   );
+
     const renderTruckRow = ({ item }) => (
-        <TouchableOpacity style={styles.truckRow} onPress={() => handleTruckPress(item.id)}>
-          <Text style={styles.truckNumber}>{item.truckNumber}</Text>
-          <Text style={styles.truckOrder}>{item.truckOrder}</Text>
-          <Text style={styles.currentLocation}>{item.currentLocation}</Text>
-          <Text style={styles.currentStatus}>{item.currentStatus}</Text>
-        </TouchableOpacity>
-      );
-    const handleTruckClick = (truckId) => {
+      <TouchableOpacity style={styles.truckRow} onPress={() => handleTruckPress(item.id)}>
+        <Text style={styles.truckNumber} numberOfLines={1} >{item.vehicleId}</Text>
+        <View style = {styles.truckDetailsContainer}>
+        <Text style={styles.truckAddress} numberOfLines={1}>{item.address}</Text>
+        <Text style={styles.currentLocation} numberOfLines={1} >{item.latitude}, {item.longitude}</Text>
+        <Text style={styles.dateTime} numberOfLines={1} >{item.dateTime}</Text>
+        <Text style={styles.dailyMileage} numberOfLines={1} >{item.dailyMileage}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+    const handleTruckPress = (truckId) => {
         navigation.navigate('TruckDetails', { truckId });
       };
   
@@ -56,13 +104,25 @@ const LandingPage = () => {
           onChangeText={handleSearch}
         />
         <View style={styles.tableHeaderContainer}>
-          <Text style={styles.tableHeaderText}>Truck Number</Text>
-          <Text style={styles.tableHeaderText}>Truck Order</Text>
-          <Text style={styles.tableHeaderText}>Current Location</Text>
-          <Text style={styles.tableHeaderText}>Current Status</Text>
+          <Text style={styles.tableHeaderText}>Vehicle ID</Text>
+          <Text style={styles.tableHeaderText}>Location</Text>
+          <Text style={styles.tableHeaderText}>Current Lat, Long</Text>
+          <Text style={styles.tableHeaderText}>Date Time</Text>
+          <Text style={styles.tableHeaderText}>Daily Mileage</Text>
+
         </View>
-        <View style={styles.tableLine} />
-        {/* <TouchableOpacity
+        {/* <ScrollView horizontal contentContainerStyle={styles.tableContainer}> */}
+        <FlatList
+        data={trucksData.onlineDataList}
+        renderItem={renderTruckRow}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.table}
+        
+      />
+      {/* </ScrollView> */}
+      
+        {/* <View style={styles.tableLine} />
+        <TouchableOpacity
         onPress={() => handleTruckClick(1)}
         style={styles.truckRow}
       >
@@ -71,12 +131,13 @@ const LandingPage = () => {
         <Text style={styles.currentLocation}>Current Location</Text>
         <Text style={styles.currentStatus}>Current Status</Text>
       </TouchableOpacity> */}
-        {/* <FlatList
-          data={filteredTrucks}
-          renderItem={renderTruckItem}
-          keyExtractor={(item) => item.truckNumber}
-        /> */}
-        {trucksData.map(truck => (
+      {/* <FlatList
+        data={trucksData.onlineDataList}
+        renderItem={renderTruckRow}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.truckList}
+      /> */}
+        {/* {trucksData.onlineDataList.map(truck => (
         <TouchableOpacity
         key={truck.truckNumber}
         onPress={() => handleTruckClick(truck.truckNumber)}
@@ -87,8 +148,18 @@ const LandingPage = () => {
           <Text style={styles.currentLocation}>{truck.currentLocation}</Text>
           <Text style={styles.currentStatus}>{truck.currentStatus}</Text>
       </TouchableOpacity>
-        ))}
+        ))} */}
       </View>
+    //   <View style={styles.container}>
+    //   <Text style={styles.heading}>Truck Dashboard</Text>
+    //   <FlatList
+    //     data={trucksData.onlineDataList}
+    //     renderItem={renderTruckRow}
+    //     keyExtractor={(item, index) => index.toString()}
+    //     style={styles.table}
+    //   />
+    // </View>
+
     );
   };
   
@@ -122,8 +193,12 @@ const LandingPage = () => {
         fontWeight: 'bold',
         fontSize: 16,
       },
+      truckDetailsContainer: {
+        flex: 4,
+        marginLeft: 16,
+      },
       truckItemContainer: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         marginBottom: 8,
       },
       truckItemText: {
@@ -131,75 +206,68 @@ const LandingPage = () => {
         fontSize: 16,
       },
       tableLine: {
-        borderBottomWidth: -1,
+        borderBottomWidth: 1,
         borderBottomColor: '#FC6D26',
         marginBottom: 8,
       },
       truckRow: {
         flexDirection: 'row',
+        alignItems: 'center',
         justifyContent: 'space-between',
+        paddingVertical: 0,
         borderBottomWidth: 1,
         borderBottomColor: '#FC6D26',
         marginBottom: 8,
       },
       truckNumber: {
         flex: 1,
-        fontSize: 16,
+        width: 0,
+        fontSize: 14,
         fontWeight: 'bold',
         color: '#000000',
+        overflow: 'hidden',
       },
       truckOrder: {
-        flex: 1,
-        fontSize: 16,
+        flex: 2,
+        marginBottom: 4,
+        fontSize: 14,
         color: '#000000',
+        overflow: 'hidden',
       },
-      currentLocation: {
+      truckAddress: {
         flex: 1,
-        fontSize: 16,
+        // width: 80,
+        fontSize: 14,
         color: '#000000',
+        overflow: 'hidden',
+      },
+      dateTime: {
+        flex: 1,
+        fontSize: 14,
+        // width : 80,
+        color: '#000000',
+        overflow: 'hidden',
+      },
+      dailyMileage: {
+        flex: 1,
+        fontSize: 14,
+        color: '#000000',
+        overflow: 'hidden',
       },
       currentStatus: {
         flex: 1,
-        fontSize: 16,
+        fontSize: 14,
         color: '#000000',
+        overflow: 'hidden',
+        // width: 80,
       },
-    // container: {
-    //   flex: 1,
-    //   padding: 16,
-    // },
-    // heading: {
-    //   fontSize: 30,
-    //   fontWeight: 'bold',
-    //   color: '#FC6D26',
-    //   textAlign: 'center',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   marginBottom: 16,
-    // },
-    // searchBar: {
-    //   height: 40,
-    //   borderWidth: 1,
-    //   borderRadius: 8,
-    //   paddingHorizontal: 16,
-    //   marginBottom: 16,
-    // },
-    // tableHeaderContainer: {
-    //   flexDirection: 'row',
-    //   marginBottom: 8,
-    // },
-    // tableHeaderText: {
-    //   flex: 1,
-    //   fontWeight: 'bold',
-    //   fontSize: 16,
-    // },
-    // truckItemContainer: {
-    //   flexDirection: 'row',
-    //   marginBottom: 8,
-    // },
-    // truckItemText: {
-    //   flex: 1,
-    //   fontSize: 16,
-    // },
+      table: {
+        flex: 1,
+        marginTop: 16,
+      },
+      tableContainer: {
+        width: '100%',
+      },
   });
 
 export default LandingPage;
---
+++ components/apiService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// Fetch truck data from API
+export const fetchTruckData = async () => {
+  try {
+    const response = await axios.get('http://zingtrack.com/GPSRestWebService/rest/GetOnlineData/json?API_KEY=CST22ZTRNIPLMRAPI&VEHICLE_ID=ALL');
+    return response.data; 
+  } catch (error) {
+    console.error('Failed to fetch truck data:', error);
+    throw error;
+  }
+};
